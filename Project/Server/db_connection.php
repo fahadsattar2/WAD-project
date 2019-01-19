cc94304bca6c73c90ee2dabbbf16c28041557ebf
Junaid Ahmed <junaidkol09@gmail.com>
2019-01-19 22:17:52 +0500

    Connection to database made!!!

--- Project/Server/db_connection.php
+++ Project/Server/db_connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = mysqli_connect("localhost",root,"","rackup");
+$connection = mysqli_connect("localhost","root","","rackup");
 
 if(!$connection){
     die("Connection to database failed".mysqli_connect_error());
