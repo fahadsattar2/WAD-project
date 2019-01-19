e2e842c47764101ea96e39cee9ed66785f881a80
Faisal Jawad <faisaljawad1@gmail.com>
2019-01-20 00:51:51 +0500

    Insertion code updated.

--- Project/Functions/db_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$connection = mysqli_connect("localhost","root","","rackup");
-
-if(!$connection){
-    die("Connection to database failed".mysqli_connect_error());
-}
-else
-    echo "Connection to database successful";
