78f4e991d0e975f308c612a678e57f449db7fa58
Sana Anjum <sanaaaanjum22@gmail.com>
2019-01-20 19:16:00 +0500

    line added

--- Project/index.php
+++ Project/index.php
@@ -1,13 +1,6 @@
 
 <?php
-    function debug_to_console( $data ) {
-        $output = $data;
-        if ( is_array( $output ) )
-            $output = implode( ',', $output);
-
-        echo "<script>console.log( 'Printing: " . $output . "' );</script>";
-    }
-   /* function successSignUp()
+   function successSignUp()
     {
         echo"<div class=\"modal fade\" id=\"LoginModal\" role=\"dialog\">";
         echo"<div class=\"modal-dialog\">";
@@ -21,14 +14,14 @@
         echo"</div>";
         echo"</div>";
         echo"</div>";
-    }*/
+    }
 
 
     require_once "Server/db_connection.php";
     $errors=array();
     if(isset($_POST['sgn_signup_btn']))
     {
-        debug_to_console("Hello");
+        echo "Hello";
         $fname = $_POST['sgn_firstname'];
         $lname = $_POST['sgn_lastname'];
         $mail = $_POST['sgn_email'];
@@ -59,8 +52,13 @@
             $insert_user = "insert into user (first_name,last_name,email,password,rating,DOB) VALUES ('$fname','$lname','$mail','$pass','0.0','$dob');";
             $insert = mysqli_query($connection, $insert_user);
             if($insert){
+
+                successSignUp();
+                header("location: ".$_SERVER['PHP_SELF']);
+
                 // successSignUp();
                 //header('location: profile.php');
+
 
             }
 
@@ -68,7 +66,13 @@
     }
     if(isset($_POST['sin_signin_btn']))
     {
-        debug_to_console("Sign-in Button clicked!!!!");
+        function debug_to_console( $data ) {
+            $output = $data;
+            if ( is_array( $output ) )
+                $output = implode( ',', $output);
+
+            echo "<script>console.log( 'Printing: " . $output . "' );</script>";
+        }
         $email=$_POST['lgn_email'];
         $pass=$_POST['lgn_pass'];
         if (empty($email)) {
@@ -77,13 +81,19 @@
         if (empty($pass)) {
             array_push($errors, "Password is required");
         }
+
         if (count($errors) == 0) {
             $password = md5($pass);
             $query = "SELECT * FROM user WHERE email='$email' AND password='$pass'";
-            $results = mysqli_query($db, $query);
+            $results = mysqli_query($connection, $query);
             if (mysqli_num_rows($results) == 1)
             {
+
+                //header('location: index.php');
+               // successSignUp();
+
                 header('location: profile.php');
+
             }
             else {
                 array_push($errors, "Wrong username/password combination");
@@ -221,7 +231,7 @@
                     <h4><span class="glyphicon glyphicon-log-in"></span> Login</h4>
                 </div>
                 <div class="modal-body" style="padding:40px 50px;">
-                    <form role="form">
+                    <form role="form" method="post">
                         <div class="input-container">
                             <i class="fa fa-envelope icon"></i>
                             <input class="input-field" type="email" placeholder="Email" name="lgn_email">
