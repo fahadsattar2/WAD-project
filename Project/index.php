4a8699e1ef5f6a98678b44def50786a22e4a5736
Sana Anjum <sanaaaanjum22@gmail.com>
2019-01-20 19:38:16 +0500

    line added

--- Project/index.php
+++ Project/index.php
@@ -1,107 +1,96 @@
-
 <?php
-   function successSignUp()
-    {
-        echo"<div class=\"modal fade\" id=\"LoginModal\" role=\"dialog\">";
-        echo"<div class=\"modal-dialog\">";
-        echo"<div class=\"modal-content\">";
-        echo"<div class=\"modal-body\" style=\"padding:40px 50px;\">";
-        echo"</div>";
-        echo"<div class=\"modal-header\" style=\"padding:35px 50px;\">";
-        echo"<h4><span class=\"glyphicon glyphicon-log-ok\"></span>Sign Up was SuccessFul</h4>";
-        echo"</div>";
-        echo"<div class=\"modal-body\" style=\"padding:40px 50px;\"><button type=\"submit\" onclick=\"window.location.href='profile.php'\" class=\"btn\" name=\"btn_profileRedirect\">Take Me to My Profile</button></div>";
-        echo"</div>";
-        echo"</div>";
-        echo"</div>";
+function debug_to_console( $data ) {
+    $output = $data;
+    if ( is_array( $output ) )
+        $output = implode( ',', $output);
+
+    echo "<script>console.log( 'Printing: " . $output . "' );</script>";
+}
+/* function successSignUp()
+ {
+     echo"<div class=\"modal fade\" id=\"LoginModal\" role=\"dialog\">";
+     echo"<div class=\"modal-dialog\">";
+     echo"<div class=\"modal-content\">";
+     echo"<div class=\"modal-body\" style=\"padding:40px 50px;\">";
+     echo"</div>";
+     echo"<div class=\"modal-header\" style=\"padding:35px 50px;\">";
+     echo"<h4><span class=\"glyphicon glyphicon-log-ok\"></span>Sign Up was SuccessFul</h4>";
+     echo"</div>";
+     echo"<div class=\"modal-body\" style=\"padding:40px 50px;\"><button type=\"submit\" onclick=\"window.location.href='profile.php'\" class=\"btn\" name=\"btn_profileRedirect\">Take Me to My Profile</button></div>";
+     echo"</div>";
+     echo"</div>";
+     echo"</div>";
+ }*/
+
+
+require_once "Server/db_connection.php";
+$errors=array();
+if(isset($_POST['sgn_signup_btn']))
+{
+    debug_to_console("Hello");
+    $fname = $_POST['sgn_firstname'];
+    $lname = $_POST['sgn_lastname'];
+    $mail = $_POST['sgn_email'];
+    $pass = $_POST['sgn_password'];
+    $dob = $_POST['sgn_dob'];
+    $pass2 = $_POST['sgn_cnfrmpass'];
+    if(empty($fname)){
+        array_push($errors,"First name is required");
     }
-
-
-    require_once "Server/db_connection.php";
-    $errors=array();
-    if(isset($_POST['sgn_signup_btn']))
+    if(empty($lname)){
+        array_push($errors,"Last name is required");
+    }
+    if(empty($mail)){
+        array_push($errors,"Email is required");
+    }
+    if(empty($pass)){
+        array_push($errors,"Password is required");
+    }
+    if(empty($dob)){
+        array_push($errors,"Date of Birth is required");
+    }
+    if($pass!=$pass2){
+        array_push($errors,"The two passwords donot match");
+    }
+    if(count($errors)==0)
     {
-        echo "Hello";
-        $fname = $_POST['sgn_firstname'];
-        $lname = $_POST['sgn_lastname'];
-        $mail = $_POST['sgn_email'];
-        $pass = $_POST['sgn_password'];
-        $dob = $_POST['sgn_dob'];
-        $pass2 = $_POST['sgn_cnfrmpass'];
-        if(empty($fname)){
-            array_push($errors,"First name is required");
+        $password=md5($pass);
+        $insert_user = "insert into user (first_name,last_name,email,password,rating,DOB) VALUES ('$fname','$lname','$mail','$pass','0.0','$dob');";
+        $insert = mysqli_query($connection, $insert_user);
+        if($insert){
+            // successSignUp();
+            //header('location: profile.php');
+
         }
-        if(empty($lname)){
-            array_push($errors,"Last name is required");
-        }
-        if(empty($mail)){
-            array_push($errors,"Email is required");
-        }
-        if(empty($pass)){
-            array_push($errors,"Password is required");
-        }
-        if(empty($dob)){
-            array_push($errors,"Date of Birth is required");
-        }
-        if($pass!=$pass2){
-            array_push($errors,"The two passwords donot match");
-        }
-        if(count($errors)==0)
+
+    }
+}
+if(isset($_POST['sin_signin_btn']))
+{
+    debug_to_console("Sign-in Button clicked!!!!");
+    $email=$_POST['lgn_email'];
+    $pass=$_POST['lgn_pass'];
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($pass)) {
+        array_push($errors, "Password is required");
+    }
+    if (count($errors) == 0) {
+        $password = md5($pass);
+        $query = "SELECT * FROM user WHERE email='$email' AND password='$pass'";
+        $results = mysqli_query($connection, $query);
+        if (mysqli_num_rows($results) == 1)
         {
-            $password=md5($pass);
-            $insert_user = "insert into user (first_name,last_name,email,password,rating,DOB) VALUES ('$fname','$lname','$mail','$pass','0.0','$dob');";
-            $insert = mysqli_query($connection, $insert_user);
-            if($insert){
-
-                successSignUp();
-                header("location: ".$_SERVER['PHP_SELF']);
-
-                // successSignUp();
-                //header('location: profile.php');
-
-
-            }
-
+            header('location: profile.php');
+        }
+        else {
+            array_push($errors, "Wrong username/password combination");
         }
     }
-    if(isset($_POST['sin_signin_btn']))
-    {
-        function debug_to_console( $data ) {
-            $output = $data;
-            if ( is_array( $output ) )
-                $output = implode( ',', $output);
-
-            echo "<script>console.log( 'Printing: " . $output . "' );</script>";
-        }
-        $email=$_POST['lgn_email'];
-        $pass=$_POST['lgn_pass'];
-        if (empty($email)) {
-            array_push($errors, "Email is required");
-        }
-        if (empty($pass)) {
-            array_push($errors, "Password is required");
-        }
-
-        if (count($errors) == 0) {
-            $password = md5($pass);
-            $query = "SELECT * FROM user WHERE email='$email' AND password='$pass'";
-            $results = mysqli_query($connection, $query);
-            if (mysqli_num_rows($results) == 1)
-            {
-
-                //header('location: index.php');
-               // successSignUp();
-
-                header('location: profile.php');
-
-            }
-            else {
-                array_push($errors, "Wrong username/password combination");
-            }
-        }
 
 
-    }
+}
 
 ?>
 
