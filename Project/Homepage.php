f46b164a569f2af4eeaee88f3a47f6c4f3b66d03
Sana Anjum <sanaaaanjum22@gmail.com>
2019-01-21 00:20:20 +0500

    check for unique email done

--- Project/Homepage.php
+++ Project/Homepage.php
@@ -59,6 +59,15 @@ if(isset($_POST['sgn_signup_btn']))
     if($pass!=$pass2){
         array_push($errors,"The two passwords do not match");
     }
+    $user_check_query = "SELECT * FROM user WHERE email='$mail' LIMIT 1";
+    $result = mysqli_query($connection, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user) { // if user exists
+        if ($user['email'] === $mail) {
+            array_push($errors, "email already exists");
+        }
+    }
     if(count($errors)==0)
     {
         $password=md5($pass);
@@ -297,12 +306,12 @@ if(isset($_POST['sin_signin_btn']))
                             ?>
                             <div class="input-container">
                                 <i class="fa fa-user icon"></i>
-                                <input pattern="[A-Z]|[a-z]+" class="input-field" type="text" placeholder="Firstname" name="sgn_firstname" value="<?php echo $fname;?>">
+                                <input pattern="([a-zA-Z' ]+)" class="input-field" type="text" placeholder="Firstname" name="sgn_firstname" value="<?php echo $fname;?>">
                             </div>
 
                             <div class="input-container">
                                 <i class="fa fa-user icon"></i>
-                                <input pattern="[A-Z]|[a-z]+" class="input-field" type="text" placeholder="Lastname" name="sgn_lastname" value="<?php echo $lname;?>">
+                                <input pattern="([a-zA-Z' ]+)" class="input-field" type="text" placeholder="Lastname" name="sgn_lastname" value="<?php echo $lname;?>">
                             </div>
 
                             <div class="input-container">
