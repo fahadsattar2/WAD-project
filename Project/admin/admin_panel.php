059f3ee4e156b7d5d60077d4ec74e5fe322a3538
Junaid Ahmed <junaidkol09@gmail.com>
2019-01-20 21:03:01 +0500

    Admin Panel updated

--- Project/admin/admin_panel.php
+++ Project/admin/admin_panel.php
@@ -1,50 +1,23 @@
 <?php
-require_once "db_connection.php";
 
-function getSidePanel(){
-    echo "<div id=\"SidePanel\" class=\"SideNavPanel\">";
-    echo "<a class=\"closebtn\" onclick=\"closeNav()\">&times;</a>";
-    echo "<a onclick=\"UserTable()\">Users</a>";
-    echo "<a href=\"#\">Projects</a>";
-    echo "<a href=\"#\">Payment Options</a>";
-    echo "</div>";
-    echo "<div><button id=\"btnOpen\" onclick=\"openNav()\">&#9776;</button></div>";
-}
-function getUsers()
-{
-    global $connection;
-    $getQuery = '';
-    /*if(isset($_GET['SearchBar'])){
-        $user_query = $_GET['SearchBar'];
-        $getQuery = "select * from user where first_name like '%$user_query%'";
-    }*/
-    $getQuery = "select * from user";
-    $getResult = mysqli_query($connection,$getQuery);
-    $count_user = mysqli_num_rows($getResult);
-    if($count_user==0){
-        echo "<h4 class='alert-warning align-center my-2 p-2'> No Data Found </h4>";
+    require_once "db_connection.php";
+
+    function printUser()
+    {
+        global $connection;
+        $getUserQuery = "select * from user where id = '1';";
+        $queryResult = mysqli_query($connection, $getUserQuery);
+        while ($row = mysqli_fetch_assoc($queryResult)) {
+            $counter = $row['id'];
+            $fname = $row['first_name'];
+            $lname = $row['last_name'];
+            $email = $row['email'];
+            $password = $row['password'];
+            $rating = $row['rating'];
+            $dob = $row['DOB'];
+            echo "$counter,$fname,$lname,$email,$password,$rating,$dob";
+        }
     }
-    while($row = mysqli_fetch_assoc($getResult)){
-        $counter = $row['id'];
-        $fname = $row['pro_title'];
-        $lname = $row['pro_price'];
-        $email = $row['pro_image'];
-        $password = $row[''];
-        $rating = $row[''];
-        $dob = $row[''];
-        echo "<tr>
-            <td class =\"col - xl - 1 col - lg - 1 col - md - 1 col - sm - 1 col - 1\"><button>New</button></td>
-            <td class=\"col - xl - 1 col - lg - 1 col - md - 1 col - sm - 1 col - 1\">Counter</td>
-            <td class=\"col - xl - 1 col - lg - 1 col - md - 1 col - sm - 1 col - 1\">Username</td>
-            <td class=\"col - xl - 1 col - lg - 1 col - md - 1 col - sm - 1 col - 1\">Email</td>
-            <td class=\"col - xl - 1 col - lg - 1 col - md - 1 col - sm - 1 col - 1\">Password</td>
-            <td class=\"col - xl - 1 col - lg - 1 col - md - 1 col - sm - 1 col - 1\">Rating</td>
-            <td class=\"col - xl - 1 col - lg - 1 col - md - 1 col - sm - 1 col - 1\">Skills</td>
-            <td class=\"col - xl - 1 col - lg - 1 col - md - 1 col - sm - 1 col - 1\">Edit</td>
-            <td class=\"col - xl - 1 col - lg - 1 col - md - 1 col - sm - 1 col - 1\">Delete</td>
-            </tr>";
-    }
-}
 ?>
 
 <!DOCTYPE html>
@@ -57,25 +30,25 @@ function getUsers()
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
 
-    <title>Rack-Up Admin Panel</title>
-
     <style>
-        body
-        {
-            font-family: 'Ubuntu';
+        body {
+            font-family: "Ubuntu";
         }
-        .SideNavPanel {
+
+        .sidenav {
             height: 100%;
             width: 0;
             position: fixed;
+            z-index: 1;
             top: 0;
             left: 0;
-            background-color: black;
+            background-color: #111;
             overflow-x: hidden;
             transition: 0.5s;
             padding-top: 60px;
         }
-        .SideNavPanel a {
+
+        .sidenav a {
             padding: 8px 8px 8px 32px;
             text-decoration: none;
             font-size: 25px;
@@ -83,68 +56,43 @@ function getUsers()
             display: block;
             transition: 0.3s;
         }
-        .SideNavPanel a:hover {
-            color: white;
+
+        .sidenav a:hover {
+            color: #f1f1f1;
         }
-        .SideNavPanel .closebtn {
+
+        .sidenav .closebtn {
             position: absolute;
             top: 0;
             right: 25px;
+            font-size: 36px;
             margin-left: 50px;
         }
-        #btnOpen {
-            transition: margin-left .75s;
-            font-size: x-large;
-            cursor: pointer;
-            background-color: transparent;
-            border: none;
-        }
-        @media screen and (max-height: 500px) {
-            .SideNavPanel {padding-top: 15px;}
-            .SideNavPanel a {font-size: 18px;}
-        }
-        #SearchBar{
-            display: none;
-        }
-        #mainBody{
-            text-align: center;
+
+        @media screen and (max-height: 450px) {
+            .sidenav {padding-top: 15px;}
+            .sidenav a {font-size: 18px;}
         }
     </style>
 </head>
-<body>
-<div class="container-fluid">
-    <?php
-    getSidePanel();
-    ?>
-
-    <table id="mainBody" class="table table-bordered offset-2 offset-xl-2 offset-lg-2">
-        <tr>
-            <p><input type="text" id="SearchBar" placeholder="Search.."/></p>
-            <?php
-            getUsers();
-            ?>
-        </tr>
-    </table>
-</div>
-
 <script>
     function openNav() {
-        document.getElementById("SidePanel").style.width = "250px";
-        document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+        document.getElementById("mySidenav").style.width = "250px";
     }
 
     function closeNav() {
-        document.getElementById("SidePanel").style.width = "0";
-        document.getElementById("btnOpen").style.marginLeft= "0";
-        document.body.style.backgroundColor = "white";
+        document.getElementById("mySidenav").style.width = "0";
+    }
+    function call_printName_php(){
+        var result = "<?php printUser() ?>"
+        alert(result);
+        return false;
     }
     function UserTable()
     {
         closeNav();
-        document.getElementById("SearchBar").style.display="block";
-        var body = document.getElementById("mainBody");
-        body.innerHTML = "<thead>"+
-            "<tr>"+
+        var body = document.getElementById("UserTableBody");
+        body.innerHTML = "<tr>"+
             "<th class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\"><button>New</button></th>"+
             "<th class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\">Counter</th>"+
             "<th class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\">Username</th>"+
@@ -154,9 +102,21 @@ function getUsers()
             "<th class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\">Skills</th>"+
             "<th class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\">Edit</th>"+
             "<th class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\">Delete</th>"+
-            "</tr>"+
-            "</thead>";
+            "</tr>";
     }
 </script>
+<body>
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a onclick="UserTable()">Users</a>
+        <a href="#">Projects</a>
+        <a href="#">Payments</a>
+    </div>
+
+    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+
+    <table id="UserTableBody" class="table table-bordered offset-2 offset-xl-2 offset-lg-2">
+    </table>
+
 </body>
 </html>
