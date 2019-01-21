431ad9eef9a1596eb8c5664a33abb65b8550ca18
mustaqeem7 <mustaqeem71110@ucp.edu.pk>
2019-01-21 18:50:37 +0500

    Navbar made with Bootstrap

--- Project/Functions/functions.php
+++ Project/Functions/functions.php
@@ -1,20 +1,18 @@
 <?php
     function top_header()
     {
-        echo "<div class=\"topnav col\">
-                <a href=\"#\">Logo</a>
-                <a class=\"active\" href=\"#\">Home</a>
-                <a href=\"#\">Projects</a>
-                <a href=\"#\">About Us</a>
-                <a href=\"#\">Contact Us</a>
-                <div class=\"search-container\">
+        echo "<div class=\"topnav col col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12\">
+                <a class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\" href=\"#\">Logo</a>
+                <a class=\"active col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\" href=\"#\">Home</a>
+                <a class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\" href=\"#\">Projects</a>
+                <div class=\"search-container col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3\">
                     <form>
-                        <input type=\"text\" placeholder=\"Search..\" name=\"search\">
-                        <button type=\"submit\">Submit</button>
+                        <input class=\"search-container col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8\" type=\"text\" placeholder=\"Search..\" name=\"search\">
+                        <button class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3\" type=\"submit\">Submit</button>
                     </form>
                 </div>
                 
-                <a href=\"#profile\" style=\"float: right;\"><i class=\"fas fa-user-tie\"></i></a>
+                <a class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\" href=\"#profile\" style=\"float: right;\"><i class=\"fas fa-user-tie\"></i></a>
                 </div>";
     }
 ?>
