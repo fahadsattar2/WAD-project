a3a98f03d065eae4ee4b23b0826b8bd23522d3fa
mustaqeem7 <mustaqeem71110@ucp.edu.pk>
2019-01-30 17:52:06 +0500

    Contact Us page all errors Resolved and Finalized

--- Project/ContactUs.php
+++ Project/ContactUs.php
@@ -1,207 +1,117 @@
-<?php
-
-function top_header()
-{
-    echo "<div class=\"header col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12\">
-          <a class=\"col-xl-1 col-lg-2 col-md-2 col-sm-12 col-12\" href=\"#\">Logo</a>
-          <a class=\"col-xl-1 col-lg-2 col-md-2 col-sm-12 col-12\" href=\"#home\">Home</a>
-          <a class=\"col-xl-1 col-lg-2 col-md-2 col-sm-12 col-12\" href=\"#about\">About</a>
-          <a class=\"col-xl-1 col-lg-2 col-md-2 col-sm-12 col-12\" href=\"#contact\">Contact</a>
-          <div class=\"search-container col-xl-2 col-lg-3 col-md-3 col-sm-12 col-12\">
-                <form class=\"col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12\" action=\"/action_page.php\">
-                    <input class=\"search-container col-xl-9 col-lg-12 col-md-8 col-sm-12 col-12\" type=\"text\" placeholder=\"Search..\" name=\"search\">
-                    <button class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3\" type=\"submit\"><i class=\"fa fa-search\"></i></button>
-                </form>
-          </div>
-    </div>";
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
-<link rel="stylesheet" href="css/bootstrap.css">
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="style.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-<link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet'>
+<head>
+    <title>Contact Us</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 
 
-<style>
-    .body_font{
-        font-family:Ubuntu;
-        color: white;
-    }
-    img {
-        width: 100%;
-        height: auto;
-    }
-    /*Below Styling for header*/
-    * {box-sizing: border-box;}
-
-    body {
-        margin: 0;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-
-    .header {
-        overflow: hidden;
-        top: 0;
-        background-color: #354763;
-    }
-
-    .header a {
-        float: left;
-        display: block;
-        color: black;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-size: 17px;
-    }
-
-    .header a:hover {
-        background-color: #ddd;
-        color: black;
-    }
-
-    .header a.active {
-        background-color: #2196F3;
-        color: white;
-    }
-
-    .header .search-container {
-        float: right;
-    }
-
-    .header input[type=text] {
-        padding: 6px;
-        margin-top: 8px;
-        font-size: 17px;
-        border: none;
-    }
-
-    .header .search-container button {
-        float: right;
-        padding: 6px 10px;
-        margin-top: 8px;
-        margin-right: 16px;
-        background: #ddd;
-        font-size: 17px;
-        border: none;
-        cursor: pointer;
-    }
-
-    .header .search-container button:hover {
-        background: #ccc;
-    }
-
-    @media screen and (max-width: 600px) {
-        .header .search-container {
-            float: none;
+    <style>
+        .body_font{
+            font-family:Ubuntu;
+            color: white;
         }
-        .header a, .header input[type=text], .header .search-container button {
-            float: none;
-            display: block;
-            text-align: left;
+        img {
             width: 100%;
+            height: auto;
+        }
+
+        body {
             margin: 0;
-            padding: 14px;
-        }
-        .header input[type=text] {
-            border: 1px solid #ccc;
+            font-family: Arial, Helvetica, sans-serif;
         }
 
-        .header_top_padding{
-            padding-top: 30px;
-        }
-    }
-</style>
+    </style>
 </head>
+
 <body>
-<?php
-top_header();
-?>
-<div>
-    <img src="Images/contact-us.jpg" alt="contact-us_Banner">
-</div
+    <?php
+    include "Functions/functions.php";
+    web_header();
+    ?>
 
-<div class="container-fluid">
-    <div class="container col-xl-8 col-lg-8 col-md-7 col-sm-10 col-10 offset-1 offset-xl-1 offset-lg-1 offset-md-1 offset-sm-1 my-5">
-        <div>
-            <p>
-            <h1>Give us a Chance to Help You</h1>
-            <h3>Fill out the following form and Let us Know what bothers you :</h3>
-            </p>
-        </div>
-        <div>
-            <p>
-                <label for="txtEmail">E-mail :</label>
-            </p>
-            <input type="text" id="txtEmail" name="txtEmail" placeholder="Enter your E-mail">
-        </div>
-
-        <div>
-            <p>
-                <label for="txtSubject">Subject :</label>
-            </p>
-            <input type="text" id="txtSubject" name="txtSubject" placeholder="Enter Subject">
-        </div>
-
-        <div>
-            <p>
-                <label for="txtMessage">Message :</label>
-            </p>
-            <textarea id="txtMessage" name="txtMessage">
-                </textarea>
-        </div>
-
+    <div>
+        <img src="Images/contact-us.jpg" alt="contact-us_Banner">
     </div>
 
-    <div class="container col-xl-3 col-lg-3 col-md-4 col-sm-10 col-10 offset-1 my-5" style="text-align: left">
+    <div class="container-fluid col-lg-10 col-md-10 col-sm-10 col-10 offset-1 offset-xl-1 offset-lg-1 offset-md-1 offset-sm-1">
+        <div class="row">
+            <div class="container-fluid col-xl-7 col-lg-7 col-md-7 col-sm-10 col-10 offset-1 offset-xl-1 offset-lg-1 offset-md-1 offset-sm-1 my-5">
+                <div>
+                    <p>
+                        <h1>Give us a Chance to Help You</h1>
+                        <h5>Fill out the following form and Let us Know what bothers you :</h5>
+                    </p>
+                </div>
 
-        <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 offset-xl-1 offset-lg-1 offset-md-1 offset-sm-1" style="margin-bottom: 5%;">
-            <p>
-            <h3>Contact with us :</h3>
-            <h4 style="text-align: left">For Support and Any Query<br>Contact us at <a href="#">contact@rackup.pk</a></h4>
-            </p>
-        </div>
+                <div>
+                    <p>
+                        <label for="txtEmail">E-mail :</label>
+                    </p>
+                    <input type="text" id="txtEmail" name="txtEmail" placeholder="Enter your E-mail">
+                </div>
 
-        <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 offset-xl-1 offset-lg-1 offset-md-1 offset-sm-1">
-            <p>
-            <h3>Where to find us?</h3>
-            <h4 style="text-align: left"><a href="https://www.google.com/maps/place/University+of+Central+Punjab/@31.4469043,74.2660429,17z/data=!3m1!4b1!4m5!3m4!1s0x3919017432b1835b:0xe396992a5b05891c!8m2!3d31.4468997!4d74.2682316">UCP Lahore</a>
-                <br>1 - Khayaban-e-Jinnah Road
-                <br>Johar Town, Lahore, Pakistan
-                <br>Zip : 54000</h4>
-            </p>
-        </div>
+                <div>
+                    <p>
+                        <label for="txtSubject">Subject :</label>
+                    </p>
+                    <input type="text" id="txtSubject" name="txtSubject" placeholder="Enter Subject">
+                </div>
 
-        <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 offset-xl-1 offset-lg-1 offset-md-1 offset-sm-1">
-            <p>
-            <h3>Ways to Contact us:</h3>
-            <div style="font-size:xx-large">
-                <a href="http://www.facebook.com"><i class="fab fa-facebook-square icon_color"></i></a>
-                <a href="http://www.gmail.com"><i class="fab fa-google-plus-square icon_color"></i></a>
-                <a href="http://www.instagram.com"><i class="fab fa-instagram icon_color"></i></a>
-                <a href="http://www.twitter.com"><i class="fab fa-twitter-square icon_color"></i></a>
+                <div>
+                    <p>
+                        <label for="txtMessage">Message :</label>
+                    </p>
+                    <textarea id="txtMessage" name="txtMessage"></textarea>
+                </div>
             </div>
-            </p>
+
+            <div class="container-fluid col-xl-3 col-lg-3 col-md-4 col-sm-10 col-10 offset-1 my-5" style="text-align: left">
+                <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 offset-xl-1 offset-lg-1 offset-md-1 offset-sm-1" style="margin-bottom: 5%;">
+                    <p>
+                    <h5>Contact with us :</h5>
+                    <h6 style="text-align: left">For Support and Any Query<br>Contact us at <a href="#">contact@rackup.pk</a></h6>
+                    </p>
+                </div>
+
+                <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 offset-xl-1 offset-lg-1 offset-md-1 offset-sm-1">
+                    <p>
+                        <h5>Where to find us?</h5>
+                        <h6 style="text-align: left"><a href="https://www.google.com/maps/place/University+of+Central+Punjab/@31.4469043,74.2660429,17z/data=!3m1!4b1!4m5!3m4!1s0x3919017432b1835b:0xe396992a5b05891c!8m2!3d31.4468997!4d74.2682316">UCP Lahore</a>
+                        <br>1 - Khayaban-e-Jinnah Road
+                        <br>Johar Town, Lahore, Pakistan
+                        <br>Zip : 54000</h6>
+                    </p>
+                </div>
+
+                <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col-10 offset-xl-1 offset-lg-1 offset-md-1 offset-sm-1">
+                    <p>
+                        <h5>Ways to Contact us:</h5>
+                        <div style="font-size:xx-large">
+                            <a href="http://www.facebook.com"><i class="fab fa-facebook-square icon_color"></i></a>
+                            <a href="http://www.gmail.com"><i class="fab fa-google-plus-square icon_color"></i></a>
+                            <a href="http://www.instagram.com"><i class="fab fa-instagram icon_color"></i></a>
+                            <a href="http://www.twitter.com"><i class="fab fa-twitter-square icon_color"></i></a>
+                        </div>
+                    </p>
+                </div>
+            </div>
         </div>
-
     </div>
-</div>
 
-<div class="container-fluid" id="location-Images">
-    <img src="Images/location.PNG" alt="Location_Banner">
-</div>
+    <div class="container-fluid" id="location-Images">
+        <img src="Images/location.PNG" alt="Location_Banner">
+    </div>
 
-<?php
-include "Functions/functions.php";
-web_footer();
-?>
+    <?php
+    web_footer();
+    ?>
 
 </body>
 </html>
--- Project/Functions/functions.php
+++ Project/Functions/functions.php
@@ -1,19 +1,39 @@
 <?php
     function web_header()
     {
-        echo "<div class=\"topnav collapsefixer col col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12\">
-                <a class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\" href=\"#\">Logo</a>
-                <a class=\"active col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\" href=\"#\">Home</a>
-                <a class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\" href=\"#\">Projects</a>
-                <div class=\"search-container col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3\">
-                    <form>
-                        <input class=\"search-container col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8\" type=\"text\" placeholder=\"Search..\" id=\"search-bar\" name=\"search-bar\" onkeyup=\"searchResult(this.value)\">
-                        <button class=\"col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3\" type=\"submit\">Submit</button>
-                    </form>
+        echo "<nav class=\"navbar navbar-expand-lg bg-dark navbar-dark\">
+    <a class=\"navbar-brand\" href=\"#\"><img src=\"Images\Rackup-Logo(100X35).png\" alt=\"RackUp international\"/></a>
+
+    <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"true\" aria-label=\"Toggle navigation\">
+        <span class=\"navbar-toggler-icon\"></span>
+    </button>
+
+    <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">
+        <ul class=\"navbar-nav mr-auto\">
+            <li class=\"nav-item active\">
+                <a class=\"nav-link\" href=\"#\">Home</a>
+            </li>
+            <li class=\"nav-item\">
+                <a class=\"nav-link\" href=\"#\">Projects</a>
+            </li>
+            <form class=\"form-inline my-2 my-lg-0\">
+                <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Search\" aria-label=\"Search\" id=\"Search-bar\" name=\"Search-bar\" onkeyup=\"searchResult(this.value)\">
+                <button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\">Search</button>
+            </form>
+
+            <li class=\"nav-item dropdown\" style=\"float: right; right: 7%; position: absolute;\">
+                <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                    Profile
+                </a>
+                <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">
+                    <a class=\"dropdown-item\" href=\"profile.php\">Profile</a>
+                    <hr>
+                    <a class=\"dropdown-item\" href=\"#\">Log Out</a>
                 </div>
-                
-                <a class=\"col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1\" href=\"#profile\" style=\"float: right;\"><i class=\"fas fa-user-tie\"></i></a>
-                </div>";
+            </li>
+        </ul>
+    </div>
+</nav>";
     }
 
     function web_footer()
